2e0ed327021a5e4923dd23aec50a67626bf21e3b
Waled Wael <waledwael0@gmail.com>
2023-06-26 13:05:29 -0700

    First Task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,3 +1,4 @@
+#include "sort.h"
 /**
  * bubble_sort -  a function that sorts an array of integers in
  * ascending order using the Bubble sort algorithm
@@ -19,6 +20,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				print_array(array, size);
+				printf("\n");
 			}
 		}
 	}
