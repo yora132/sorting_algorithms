448944bcba4b1d893c6408d21f63cfeb41cff1af
Waled Wael <waledwael0@gmail.com>
2023-06-27 11:27:16 -0700

    Done

--- sort.h
+++ sort.h
@@ -1,11 +1,8 @@
+#ifndef SORT_H
+#define SORT_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-void bubble_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
-
-
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -19,3 +16,8 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+
+#endif
