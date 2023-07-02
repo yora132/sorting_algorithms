4c2a4ea5bcd29beb42258fe77271d047f2ca29cb
yora132 <yarasalah132@gmail.com>
2023-07-02 09:53:00 -0700

    done

--- sort.h
+++ sort.h
@@ -22,4 +22,5 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
 #endif
